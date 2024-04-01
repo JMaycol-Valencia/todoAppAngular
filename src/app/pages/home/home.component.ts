d13fa0415255285e6a83a372026c49b91297b6a5
JMaycol-Valencia <maycolvalencia50@gmail.com>
2024-04-01 12:17:32 -0400

    uso de ngfor en objetos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +10,37 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  lista = signal( [
-    'Instalar angular',
-    'Crear proyecto',
-    'Crear componentes',
-    'Crear servicios'
+  lista = signal<Task[]>( [
+    {
+      id: Date.now(),
+      title: "Crear Proyecto",
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: "Crear Componentes",
+      completed: true
+    },
+
   ])
 
   changeHandler(event:  Event){
     const input = event.target as HTMLInputElement;
     const newTarea = input.value;
-    //usaremos el metodo update , el cual nos dara el valor previo y debemos modificar el valor mediante un arrowfunction
-    this.lista.update((lista) => [...lista, newTarea]);
+    //Usamos la funcion que nos ayuda a agregar las tareas
+    this.addTask(newTarea);
     //Si queremos recetear el imput solo agregar la siguiente instrucicon
     input.value = "";
+  }
+
+  addTask(title:string){
+    const newTarea = {
+      id: Date.now(),
+      title,
+      completed:false
+    };
+    //usaremos el metodo update , el cual nos dara el valor previo y debemos modificar el valor mediante un arrowfunction
+    this.lista.update((lista) => [...lista, newTarea]);
   }
 
   deleteTareas(index: number){
