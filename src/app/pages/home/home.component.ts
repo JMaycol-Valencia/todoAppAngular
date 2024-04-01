2adda3cb78781fa2f28b2ef01bac2e80397415a5
JMaycol-Valencia <maycolvalencia50@gmail.com>
2024-04-01 11:20:53 -0400

    directivas y ngfor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  lista = signal( [
+    'Instalar angular',
+    'Crear proyecto',
+    'Crear componentes',
+    'Crear servicios'
+  ])
 
+  changeHandler(event:  Event){
+    const input = event.target as HTMLInputElement;
+    const newTarea = input.value;
+    //usaremos el metodo update , el cual nos dara el valor previo y debemos modificar el valor mediante un arrowfunction
+    this.lista.update((lista) => [...lista, newTarea]);
+    //Si queremos recetear el imput solo agregar la siguiente instrucicon
+    input.value = "";
+  }
+
+  deleteTareas(index: number){
+    this.lista.update((lista) => lista.filter((tarea, position) => position !== index));
+  }
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,27 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {signal} from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { DividerComponent } from '../../components/divider/divider.component';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent,DividerComponent],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
+  //CREAREMOS NUESTRO PRIMER SIGNAL , LO HAREMOS LLAMANDO AL METODO SIGNAL
+  //para LLAMARLO SIEMPRE DEBEMOS EJECUTARLO
   name = signal('maycol');
   age : number = 26;
   Bienvenido : string = "Hola bienvenidos";
   items  = [
     'instalar','crear proyectos','crear componentes'
   ]
-  lista = [
+  lista = signal( [
     'Instalar angular',
     'Crear proyecto',
     'Crear componentes',
     'Crear servicios'
-  ]
+  ])
   disabled = true;
   img = 'https://w3schools.com/howto/img_avatar.png'
   person = {
@@ -33,8 +37,18 @@ export class LabsComponent {
     alert('Hola');
   }
 
+  //opcion para modificar el valor
   inputHandler(event : Event){
-    console.log(event)
+    const input = event.target as HTMLInputElement;
+    const newValor = input.value;
+    this.name.set(newValor);
+  }
+
+  changeHandler(event:  Event){
+    const input = event.target as HTMLInputElement;
+    const newVal = input.value;
+    //para cambiar un valor con signals lo mejor es hacerlo de la siguiente manera
+    this.name.set(newVal);
   }
 
   keydownHandler(event: KeyboardEvent){
